99b2b40ea30dcabc5e2cb29f9d71addf8181766d
Josue Lopes <josue10599@gmail.com>
2019-06-04 18:51:43 -0300

    Refatoração de LeilaoTest

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -6,6 +6,10 @@ import static org.junit.Assert.assertEquals;
 
 public class LeilaoTest {
 
+    private Leilao computador = new Leilao("Computador");
+    private Usuario bot = new Usuario("Bot");
+    private Usuario pc = new Usuario("Pc");
+
     /**
      * Para padronizar e melhor descrever o que um método faz a comunidade Java criou alguns padrões
      * para boas práticas na implementação de teste, uma delas é a nomeação descritiva dos métodos,
@@ -19,10 +23,8 @@ public class LeilaoTest {
     public void deve_DevolverDescrição_QuandoRecebeUmaDescricao() {
         // Criação de ambiente de Teste
         Leilao console = new Leilao("Console");
-
         // Executar ação esperada
         String descricaoRecebida = console.getDescricao();
-
         // Testar o resultado esperado
         assertEquals("Console", descricaoRecebida);
     }
@@ -30,66 +32,57 @@ public class LeilaoTest {
     @Test
     public void deve_DevolverMaiorValor_QuandoRecebeUmLance() {
         // Criação de ambiente de Teste
-        Leilao console = new Leilao("Console");
-        console.propoeLance(new Lance(new Usuario("Bot"), 200.0));
+        computador.propoeLance(new Lance(bot, 200.0));
         // Executar ação esperada
-        double maiorValorDevolvido = console.getMaiorValor();
+        double maiorValorDevolvido = computador.getMaiorValor();
         // Testar o resultado esperado
         assertEquals(200.0, maiorValorDevolvido, 0.0001);
-        /**
-         * DELTA = o Delta é o valor utilizado para a comparação de números com pontos flutuantes
-         * com ele colocamos uma margem de diferença que pode haver entre os dois valores passados,
-         * para que o computador entenda como números iguais, ou seja, se o delta que passamos for
-         * maior que a diferença entre o dois números que comparamos significa que eles são equivalentes.
-         * */
-
+        /*DELTA = o Delta é o valor utilizado para a comparação de números com pontos flutuantes
+        com ele colocamos uma margem de diferença que pode haver entre os dois valores passados,
+        para que o computador entenda como números iguais, ou seja, se o delta que passamos for
+        maior que a diferença entre o dois números que comparamos significa que eles são equivalentes.*/
     }
 
     @Test
     public void deve_DevolverMaiorValor_QuandoRecebeLancesEmOrdemCrescente() {
-        Leilao computador = new Leilao("Computador");
-        computador.propoeLance(new Lance(new Usuario("Bot"), 10.0));
-        computador.propoeLance(new Lance(new Usuario("Pc"), 100.0));
-        computador.propoeLance(new Lance(new Usuario("Machine"), 1000.0));
+        computador.propoeLance(new Lance(bot, 10.0));
+        computador.propoeLance(new Lance(pc, 100.0));
+        computador.propoeLance(new Lance(bot, 1000.0));
         double maiorValorDevolvido = computador.getMaiorValor();
         assertEquals(1000.0, maiorValorDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMaiorValor_QuandoRecebeLancesEmOrdemDescrescente() {
-        Leilao carro = new Leilao("Carro");
-        carro.propoeLance(new Lance(new Usuario("Bot"), 1000.0));
-        carro.propoeLance(new Lance(new Usuario("Pc"), 500.0));
-        carro.propoeLance(new Lance(new Usuario("Mac"), 100.0));
-        double maiorValorDevolvido = carro.getMaiorValor();
+        computador.propoeLance(new Lance(bot, 1000.0));
+        computador.propoeLance(new Lance(bot, 500.0));
+        computador.propoeLance(new Lance(pc, 100.0));
+        double maiorValorDevolvido = computador.getMaiorValor();
         assertEquals(1000.0, maiorValorDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMenorValor_QuandoRecebeUmLance() {
-        Leilao computador = new Leilao("Computador");
-        computador.propoeLance(new Lance(new Usuario("Bot"), 100.0));
+        computador.propoeLance(new Lance(bot, 100.0));
         double menorValorDevolvido = computador.getMenorValor();
         assertEquals(100.0, menorValorDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMenorValor_QuandoRecebeLancesEmOrdemCrescente() {
-        Leilao computador = new Leilao("Computador");
-        computador.propoeLance(new Lance(new Usuario("Bot"), 10.0));
-        computador.propoeLance(new Lance(new Usuario("Pc"), 100.0));
-        computador.propoeLance(new Lance(new Usuario("Machine"), 1000.0));
+        computador.propoeLance(new Lance(pc, 10.0));
+        computador.propoeLance(new Lance(bot, 100.0));
+        computador.propoeLance(new Lance(bot, 1000.0));
         double menorValorDevolvido = computador.getMenorValor();
         assertEquals(10.0, menorValorDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMenorValor_QuandoRecebeLancesEmOrdemDescrescente() {
-        Leilao carro = new Leilao("Carro");
-        carro.propoeLance(new Lance(new Usuario("Bot"), 1000.0));
-        carro.propoeLance(new Lance(new Usuario("Pc"), 500.0));
-        carro.propoeLance(new Lance(new Usuario("Mac"), 100.0));
-        double menorValorDevolvido = carro.getMenorValor();
+        computador.propoeLance(new Lance(bot, 1000.0));
+        computador.propoeLance(new Lance(pc, 500.0));
+        computador.propoeLance(new Lance(pc, 100.0));
+        double menorValorDevolvido = computador.getMenorValor();
         assertEquals(100.0, menorValorDevolvido, 0.0001);
     }
 }
