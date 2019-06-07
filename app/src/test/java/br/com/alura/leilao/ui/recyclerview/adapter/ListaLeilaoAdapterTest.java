6765cf448f2f4ace5874934dbb35325adbe5050d
Josue Lopes <josue10599@gmail.com>
2019-06-07 13:29:32 -0300

    Implementação do Mockito por meio de anotação e métodos estáticos

--- app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
+++ app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
@@ -3,7 +3,10 @@ package br.com.alura.leilao.ui.recyclerview.adapter;
 import android.content.Context;
 
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 
@@ -12,17 +15,23 @@ import br.com.alura.leilao.model.Leilao;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ListaLeilaoAdapterTest {
+
+    @Mock
+    Context context;
+    @Spy
+    ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
 
     @Test
     public void deve_AtualizarLista_QuandoRecebeNovosValores() {
-        Context context = Mockito.mock(Context.class);
-        ListaLeilaoAdapter adapter = Mockito.spy(new ListaLeilaoAdapter(context));
-        Mockito.doNothing().when(adapter).atualizaLista();
+        doNothing().when(adapter).atualizaLista();
         adapter.atualiza(Arrays.asList(new Leilao("Computador"), new Leilao("CD")));
         int numeroDeItensNaLista = adapter.getItemCount();
-        Mockito.verify(adapter).atualizaLista();
+        verify(adapter).atualizaLista();
         assertThat(numeroDeItensNaLista, is(equalTo(2)));
     }
 }
