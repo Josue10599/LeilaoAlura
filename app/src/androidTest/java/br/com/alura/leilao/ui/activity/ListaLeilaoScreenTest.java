0c9393d06bc475773cee61b9c3c046bbec7fa5cb
Josue Lopes <josue10599@gmail.com>
2019-06-10 17:33:16 -0300

    Refatoração do primeiro teste espresso da tela ListaLeilao

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoScreenTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoScreenTest.java
@@ -1,23 +1,24 @@
 package br.com.alura.leilao.ui.activity;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
 public class ListaLeilaoScreenTest {
 
     @Rule
-    public ActivityTestRule activityTestRule = new ActivityTestRule(ListaLeilaoActivity.class,
-            true, true);
+    public ActivityTestRule<ListaLeilaoActivity> activityTestRule = new ActivityTestRule<>
+            (ListaLeilaoActivity.class, true, true);
 
     @Test
     public void deve_MostrarLeilaoCadastrado_QuandoCarregarLeilaoDaApi() {
-        Espresso.onView(ViewMatchers.withText("Computador"))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(ViewMatchers.withText("Computador")).check(matches(isDisplayed()));
     }
 
 }
